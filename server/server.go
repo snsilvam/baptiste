ceb50e694ad515452696e54b5d5a15b30f77b0f9
Sergio <silva.mendieta.nicolas@gmail.com>
2023-06-03 08:27:46 -0500

    initial implementation of server package

--- server/server.go
+++ server/server.go
@@ -3,6 +3,10 @@ package server
 import (
 	"context"
 	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Config struct {
@@ -16,7 +20,7 @@ type Server interface {
 
 type Broker struct {
 	config *Config
-	//router *mux.Router
+	router *gin.
 }
 
 func (b *Broker) Config() *Config {
@@ -25,16 +29,26 @@ func (b *Broker) Config() *Config {
 
 func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 	if config.Port == "" {
-		return nil, errors.New("Port is required")
+		return nil, errors.New("port is required")
 	}
 
 	if config.DatabaseUrl == "" {
-		return nil, errors.New("Url of database is required")
+		return nil, errors.New("url of  database is requiered")
 	}
 
 	broker := &Broker{
 		config: config,
-		//router:
+		router: gin.New(),
 	}
+
 	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *gin.IRouter)) {
+	//b.router = new router
+	//binder(b, b.router)
+	log.Println("Starting server on port", b.Config().Port)
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
