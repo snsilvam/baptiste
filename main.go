45142c1830d7216f816aa25f18a01f5223d59463
Sergio <silva.mendieta.nicolas@gmail.com>
2024-09-17 14:40:03 -0500

    Se agrega manejo de cors

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"baptiste.com/config"
 	"baptiste.com/database"
 	"baptiste.com/src/gasto"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -22,9 +25,16 @@ func main() {
 	if err != nil {
 		log.Fatal("error inicializando el servidor ", err)
 	}
+	server.Router.Use(cors.Default())
 
 	gastoModule := gasto.ConstructorGastoModule(*server.Database)
 	gastoModule.RegisterGastoRoutes(server.Router)
 
+	// Crear una ruta GET en el endpoint "/hello"
+	server.Router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!. Welcome to baptiste api♥",
+		})
+	})
 	server.StartServer()
 }
